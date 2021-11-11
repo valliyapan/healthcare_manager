c0c6c981e63512277b7fd47194ab03070698216c
Thirupathy927 <dheenathiru2000@gmail.com>
2021-11-11 10:07:56 +0530

    Added Admin Page

--- db/migrate/20211026044222_create_patients.rb
+++
@@ -1,9 +0,0 @@
-class CreatePatients < ActiveRecord::Migration[6.1]
-  def change
-    create_table :patients do |t|
-      t.boolean :is_blocked
-      t.belongs_to :user, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20211026044633_create_specializations.rb
+++
@@ -1,8 +0,0 @@
-class CreateSpecializations < ActiveRecord::Migration[6.1]
-  def change
-    create_table :specializations do |t|
-      t.string :name
-      t.timestamps
-    end
-  end
-end

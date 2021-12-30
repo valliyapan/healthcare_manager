4bb6c8b2e7918aa52e1c165cdcfda16e0145431b
valliyapan <valliyapan07@gmail.com>
2021-12-30 12:24:59 +0530

    Performed debugging

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,11 +4,11 @@ class Appointment < ApplicationRecord
   belongs_to :hospital
 
   def margin
-    (self.appointment_time-DateTime.now)/3600>=7.30
+    (self.appointment_time-DateTime.now.in_time_zone("Chennai"))/3600>=7.30
   end
 
   def self.check(i,t)
-    curr_hour = DateTime.now.strftime("%H").to_i
+    curr_hour = DateTime.now.strftime("%H").to_i + 1
     ((t==0 && curr_hour > i+9) || (t>3))
   end
 end
